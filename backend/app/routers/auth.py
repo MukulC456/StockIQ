41f147bbd32f384ce1f2c1523862207f4e55d278
MukulC456 <mchoudhari2004@gmail.com>
2026-06-05 23:03:16 +0530

    my commits

---
+++ backend/app/routers/auth.py
@@ -0,0 +1,54 @@
+from fastapi import APIRouter
+from fastapi import Depends
+from fastapi import HTTPException
+
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+
+from app.models.user import User
+
+from app.schemas.user import UserCreate
+
+from app.auth.password import hash_password
+
+router = APIRouter(
+    prefix="/auth",
+    tags=["Authentication"]
+)
+
+
+@router.post("/register")
+def register_user(
+    user: UserCreate,
+    db: Session = Depends(get_db)
+):
+
+    existing_user = db.query(User).filter(
+        User.email == user.email
+    ).first()
+
+    if existing_user:
+
+        raise HTTPException(
+            status_code=400,
+            detail="Email already exists"
+        )
+
+    new_user = User(
+        username=user.username,
+        email=user.email,
+        hashed_password=hash_password(
+            user.password
+        )
+    )
+
+    db.add(new_user)
+
+    db.commit()
+
+    db.refresh(new_user)
+
+    return {
+        "message": "User created successfully"
+    }
