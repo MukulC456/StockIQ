08f2bb7586e22195b88a1e819ce3b19aa672ba95
MukulC456 <mchoudhari2004@gmail.com>
2026-06-05 23:00:16 +0530

    my commits

---
+++ backend/app/models/user.py
@@ -0,0 +1,38 @@
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from app.database import Base
+
+
+class User(Base):
+
+    __tablename__ = "users"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    username = Column(
+        String(50),
+        unique=True,
+        nullable=False
+    )
+
+    email = Column(
+        String(100),
+        unique=True,
+        nullable=False
+    )
+
+    hashed_password = Column(
+        String,
+        nullable=False
+    )
+
+    role = Column(
+        String,
+        default="staff"
+    )
---
+++ backend/app/schemas/user.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel
+from pydantic import EmailStr
+
+
+class UserCreate(BaseModel):
+
+    username: str
+
+    email: EmailStr
+
+    password: str
+
+
+class UserResponse(BaseModel):
+
+    id: int
+
+    username: str
+
+    email: EmailStr
+
+    role: str
+
+    model_config = {
+        "from_attributes": True
+    }
